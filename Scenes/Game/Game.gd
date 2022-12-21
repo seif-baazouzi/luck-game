6286a8b65d9cad0b35051b1a7a1a4634ec95f69d
seif <baazouziseif9@gmail.com>
2022-12-21 16:37:59 +0100

    save high score

--- Scenes/Game/Game.gd
+++ Scenes/Game/Game.gd
@@ -16,6 +16,7 @@ var counter = 0
 func _ready():
 	randomize()
 	VisualServer.set_default_clear_color(Color("#87CEEB"))
+	setTopScore()
 
 func _physics_process(delta):
 	generateGround()
@@ -31,7 +32,19 @@ func generateGround():
 	counter += 1
 
 func restartGame():
-	if player.global_position.y > 2000:
+	if player.global_position.y > 576:
+		var data = loadData()
+
+		var score = data["score"]
+		if data["score"] == null:
+			score = 0
+		
+		if player.score > score:
+			$UI/NewHighScore.visible = true
+			data["score"] = player.score
+			saveData(data)
+		
+		yield(get_tree().create_timer(3), "timeout")
 		get_tree().reload_current_scene()
 
 
@@ -43,3 +56,34 @@ func getGroundYPosition() -> int:
 		return 480
 	else:
 		return 384
+
+func setTopScore():
+	var data = loadData()
+	
+	var score = 0
+	if "score" in data:
+		score = data["score"]
+	
+	$UI/HighScore.text = "HIGH SCORE: " + str(score)
+
+##############################
+######### save data ##########
+##############################
+
+var dataFile = File.new()
+		
+func loadData():
+	var err = dataFile.open("user://data.json", File.READ)
+	if err:
+		return { "score": 0 }
+		
+	var dataJson = JSON.parse(dataFile.get_as_text())
+	dataFile.close()
+	
+	return dataJson.result
+
+func saveData(data):
+	dataFile.open("user://data.json", File.WRITE)
+	dataFile.store_line(JSON.print(data))
+	dataFile.close()
+
