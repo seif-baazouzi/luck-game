a472b0d2a8b4c521c7f3c1e99d875a55cec54950
seif <baazouziseif9@gmail.com>
2022-12-21 08:02:27 +0100

    add other ground types

--- Scenes/Game/Game.gd
+++ Scenes/Game/Game.gd
@@ -1,7 +1,14 @@
 extends Node2D
 
 onready var player = $Player 
-const groundChain = preload("res://Nodes/Ground/GroundChain.tscn")
+const groundChains = [
+	preload("res://Nodes/Ground/GroundChain1.tscn"),
+	preload("res://Nodes/Ground/GroundChain2.tscn"),
+	preload("res://Nodes/Ground/GroundChain3.tscn"),
+	preload("res://Nodes/Ground/GroundChain4.tscn"),
+	preload("res://Nodes/Ground/GroundChain5.tscn"),
+	preload("res://Nodes/Ground/GroundChain6.tscn"),
+]
 
 var speed = 5
 var counter = 0
@@ -17,8 +24,8 @@ func _physics_process(delta):
 func generateGround():
 	if counter == 36 * speed:
 		counter = 0
-		var groundChainInstance = groundChain.instance()
-		groundChainInstance.position = Vector2(960, get_ground_y_position())
+		var groundChainInstance = getGroundChain().instance()
+		groundChainInstance.position = Vector2(960, getGroundYPosition())
 		add_child(groundChainInstance)
 
 	counter += 1
@@ -28,7 +35,10 @@ func restartGame():
 		get_tree().reload_current_scene()
 
 
-func get_ground_y_position() -> int:
+func getGroundChain() -> Node2D:
+	return groundChains[rand_range(0, 6)]
+
+func getGroundYPosition() -> int:
 	if int(rand_range(0, 2)) == 0:
 		return 480
 	else:
